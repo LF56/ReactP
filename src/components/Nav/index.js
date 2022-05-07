bc429f18f632117aa9fb824f466dca759d4fdea3
LF56 <leah.fox7@gmail.com>
2022-05-07 19:48:49 -0400

    added nav file

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
-  const  categories = [
+  const categories = [
     { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
@@ -21,19 +21,29 @@ function Nav() {
         <ul className="flex-row">
           <li className="mx-2">
             <a href="#about" onClick={() => handleClick()}>
-              About me
+              About Me
             </a>
+          </li>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick()}>
+              Projects
+            </span>
           </li>
           <li className={"mx-2"}>
             <span onClick={() => handleClick()}>
               Contact
             </span>
           </li>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick()}>
+              Resume
+            </span>
+          </li>
           {
             categories.map((category) => (
               <li className="mx-1" key={category.name} >
                 <span onClick={() => { handleClick(); }}>
-                 {capitalizeFirstLetter(category.name)}
+                  {capitalizeFirstLetter(category.name)}
                 </span>
               </li>
             ))
