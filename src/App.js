657376e3339aa9989775bf3c73b1071ccda6f646
LF56 <leah.fox7@gmail.com>
2022-05-07 20:21:24 -0400

    fixed nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import About from './components/About';
-// import Nav from './components/Nav';
+import Nav from './components/Nav';
 
 function App() {
+  const [categories] = useState([
+    { name: 'about Me'},
+    { name: 'projects'},
+    { name: 'contact Me'},
+    { name: 'resume'},
+  ]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
-    {/* <Nav/> */}
+    <Nav
+    categories={categories}
+    setCurrentCategory={setCurrentCategory}
+    currentCategory={currentCategory}
+    contactSelected={contactSelected}
+    setContactSelected={setContactSelected}
+    />
       <main>
           <About />        
       </main>
